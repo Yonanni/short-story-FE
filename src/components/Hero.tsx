22c77648f7ff55f7dbfdba1c3ed7408d9c7ba341
YonathanMe <yniyoo@gmail.com>
2022-03-26 22:30:20 +0100

    useUser
    custom hook from context

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,15 @@
 import { Image } from "react-bootstrap";
+import { useUser } from "../context/CurrentUser";
+
 
 export default function Hero() {
+  const user = useUser()
+ 
   return (
     <div id="hero" className="mb-4">
       <Image src="/breezeHero.png" fluid />
       <div className="hero-text d-flex flex-column align-items-end">
-        <h1>Breeze Stories</h1>
+        <h1>{user}</h1>
         <p className="d-none d-sm-none d-md-block">
           A place for Storytellers and readers.
         </p>
